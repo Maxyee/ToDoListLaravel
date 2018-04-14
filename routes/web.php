d5a7f7107490fdba0b1fc20e39446f92493daff6
Maxyee <eyaminkhan00@gmail.com>
2018-04-14 07:10:58 +0600

    in this todolist project in this commited section i added how to insert data into database, and validate into the route

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,23 @@ Route::get('/tasks', function () {
 });
 
 Route::post('/task', function (Request $request) {
+    $validator = 
+    Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    if($validator->fails()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
     
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 Route::delete('/task/{id}', function ($id) {
